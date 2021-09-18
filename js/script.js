6238409373b4cce33830a6c5a60abab8ef6c7669
asselin <baptiste.asselin@orange.fr>
2021-09-18 18:35:05 +0200

    added random symbol display and html format

--- js/script.js
+++ js/script.js
@@ -1,3 +1,29 @@
-window.addEventListener("load", function(event) {
-    console.log(data);
+window.addEventListener("load", () => {
+    document.addEventListener("keydown", (event) => {
+        const display = this.document.querySelector(".display");
+        const randint = getRandomInt(data.length);
+        let listRomaji = [];
+        listRomaji.push(data[randint].romaji);
+        for(let i = 0; i < 3; i++) {
+            let element;
+            do {
+                element = data[getRandomInt(data.length)];
+            } while((listRomaji.includes(element.romaji)));
+            listRomaji.push(element.romaji);
+        }
+
+        for (let i = listRomaji.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            const temp = listRomaji[i];
+            listRomaji[i] = listRomaji[j];
+            listRomaji[j] = temp;
+        }
+          
+        display.querySelector(".symbol").textContent = data[randint].symbol;
+        display.querySelector(".romaji").textContent = `${listRomaji[0]}, ${listRomaji[1]}, ${listRomaji[2]}, ${listRomaji[3]}`;
+    });
 });
+
+getRandomInt = (max) => {
+    return Math.floor(Math.random() * max)
+}
