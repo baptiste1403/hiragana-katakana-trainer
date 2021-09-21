2a7a25f22f6181d280238f1f2ab4c859c8f3cc0c
TheDestiny45 <vgorgeon@gmail.com>
2021-09-21 21:47:54 +0200

    improved data with katakana

--- js/data.js
+++ js/data.js
@@ -17,7 +17,7 @@ data = [
         {
           "type": "hiragana",
           "symbol": "い",
-          "romaji": "I"
+          "romaji": "i"
         },
         {
           "type": "hiragana",
@@ -533,5 +533,540 @@ data = [
           "type": "hiragana2",
           "symbol": "ぢょ",
           "romaji": "djo"
+        },
+        {
+          "type": "katakana",
+          "symbol": "カ",
+          "romanji": "ka"
+        },
+        {
+          "type": "katakana",
+          "symbol": "ク",
+          "romanji": "ku"
+        },
+        {
+          "type": "katakana",
+          "symbol": "ア",
+          "romanji": "a"
+        },
+        {
+          "type": "katakana",
+          "symbol": "イ",
+          "romanji": "i"
+        },
+        {
+          "type": "katakana",
+          "symbol": "ウ",
+          "romanji": "u"
+        },
+        {
+          "type": "katakana",
+          "symbol": "エ",
+          "romanji": "e"
+        },
+        {
+          "type": "katakana",
+          "symbol": "オ",
+          "romanji": "o"
+        },
+        {
+          "type": "katakana",
+          "symbol": "キ",
+          "romanji": "ki"
+        },
+        {
+          "type": "katakana",
+          "symbol": "ケ",
+          "romanji": "ke"
+        },
+        {
+          "type": "katakana",
+          "symbol": "コ",
+          "romanji": "ko"
+        },
+        {
+          "type": "katakana",
+          "symbol": "タ",
+          "romanji": "ta"
+        },
+        {
+          "type": "katakana",
+          "symbol": "チ",
+          "romanji": "chi"
+        },
+        {
+          "type": "katakana",
+          "symbol": "ツ",
+          "romanji": "tsu"
+        },
+        {
+          "type": "katakana",
+          "symbol": "テ",
+          "romanji": "te"
+        },
+        {
+          "type": "katakana",
+          "symbol": "ト",
+          "romanji": "to"
+        },
+        {
+          "type": "katakana",
+          "symbol": "サ",
+          "romanji": "sa"
+        },
+        {
+          "type": "katakana",
+          "symbol": "シ",
+          "romanji": "shi"
+        },
+        {
+          "type": "katakana",
+          "symbol": "ス",
+          "romanji": "su"
+        },
+        {
+          "type": "katakana",
+          "symbol": "セ",
+          "romanji": "se"
+        },
+        {
+          "type": "katakana",
+          "symbol": "ソ",
+          "romanji": "so"
+        },
+        {
+          "type": "katakana",
+          "symbol": "ラ",
+          "romanji": "ra"
+        },
+        {
+          "type": "katakana",
+          "symbol": "リ",
+          "romanji": "ri"
+        },
+        {
+          "type": "katakana",
+          "symbol": "ル",
+          "romanji": "ru"
+        },
+        {
+          "type": "katakana",
+          "symbol": "レ",
+          "romanji": "re"
+        },
+        {
+          "type": "katakana",
+          "symbol": "ロ",
+          "romanji": "ro"
+        },
+        {
+          "type": "katakana",
+          "symbol": "ナ",
+          "romanji": "na"
+        },
+        {
+          "type": "katakana",
+          "symbol": "ニ",
+          "romanji": "ni"
+        },
+        {
+          "type": "katakana",
+          "symbol": "ヌ",
+          "romanji": "nu"
+        },
+        {
+          "type": "katakana",
+          "symbol": "ネ",
+          "romanji": "ne"
+        },
+        {
+          "type": "katakana",
+          "symbol": "ノ",
+          "romanji": "no"
+        },
+        {
+          "type": "katakana",
+          "symbol": "ハ",
+          "romanji": "ha"
+        },
+        {
+          "type": "katakana",
+          "symbol": "ヒ",
+          "romanji": "hi"
+        },
+        {
+          "type": "katakana",
+          "symbol": "フ",
+          "romanji": "fu"
+        },
+        {
+          "type": "katakana",
+          "symbol": "ヘ",
+          "romanji": "he"
+        },
+        {
+          "type": "katakana",
+          "symbol": "ホ",
+          "romanji": "ho"
+        },
+        {
+          "type": "katakana",
+          "symbol": "マ",
+          "romanji": "ma"
+        },
+        {
+          "type": "katakana",
+          "symbol": "ミ",
+          "romanji": "mi"
+        },
+        {
+          "type": "katakana",
+          "symbol": "ム",
+          "romanji": "mu"
+        },
+        {
+          "type": "katakana",
+          "symbol": "メ",
+          "romanji": "me"
+        },
+        {
+          "type": "katakana",
+          "symbol": "モ",
+          "romanji": "mo"
+        },
+        {
+          "type": "katakana",
+          "symbol": "ヤ",
+          "romanji": "ya"
+        },
+        {
+          "type": "katakana",
+          "symbol": "ユ",
+          "romanji": "yu"
+        },
+        {
+          "type": "katakana",
+          "symbol": "ヨ",
+          "romanji": "yo"
+        },
+        {
+          "type": "katakana",
+          "symbol": "ハ",
+          "romanji": "wa"
+        },
+        {
+          "type": "katakana",
+          "symbol": "オ",
+          "romanji": "wo"
+        },
+        {
+          "type": "katakana",
+          "symbol": "ン",
+          "romanji": "n"
+        },
+        {
+          "type": "katakana1",
+          "symbol": "ガ",
+          "romanji": "ga"
+        },
+        {
+          "type": "katakana1",
+          "symbol": "ギ",
+          "romanji": "gi"
+        },
+        {
+          "type": "katakana1",
+          "symbol": "グ",
+          "romanji": "gu"
+        },
+        {
+          "type": "katakana1",
+          "symbol": "ゲ",
+          "romanji": "ge"
+        },
+        {
+          "type": "katakana1",
+          "symbol": "ゴ",
+          "romanji": "go"
+        },
+        {
+          "type": "katakana1",
+          "symbol": "ザ",
+          "romanji": "za"
+        },
+        {
+          "type": "katakana1",
+          "symbol": "ジ",
+          "romanji": "ji"
+        },
+        {
+          "type": "katakana1",
+          "symbol": "ズ",
+          "romanji": "zu"
+        },
+        {
+          "type": "katakana1",
+          "symbol": "ゼ",
+          "romanji": "ze"
+        },
+        {
+          "type": "katakana1",
+          "symbol": "ゾ",
+          "romanji": "zo"
+        },
+        {
+          "type": "katakana1",
+          "symbol": "ダ",
+          "romanji": "da"
+        },
+        {
+          "type": "katakana1",
+          "symbol": "ジ",
+          "romanji": "dji"
+        },
+        {
+          "type": "katakana1",
+          "symbol": "ズ",
+          "romanji": "dzu"
+        },
+        {
+          "type": "katakana1",
+          "symbol": "デ",
+          "romanji": "de"
+        },
+        {
+          "type": "katakana1",
+          "symbol": "ド",
+          "romanji": "do"
+        },
+        {
+          "type": "katakana1",
+          "symbol": "バ",
+          "romanji": "ba"
+        },
+        {
+          "type": "katakana1",
+          "symbol": "ビ",
+          "romanji": "bi"
+        },
+        {
+          "type": "katakana1",
+          "symbol": "ブ",
+          "romanji": "bu"
+        },
+        {
+          "type": "katakana1",
+          "symbol": "ベ",
+          "romanji": "be"
+        },
+        {
+          "type": "katakana1",
+          "symbol": "ボ",
+          "romanji": "bo"
+        },
+        {
+          "type": "katakana1",
+          "symbol": "パ",
+          "romanji": "pa"
+        },
+        {
+          "type": "katakana1",
+          "symbol": "ピ",
+          "romanji": "pi"
+        },
+        {
+          "type": "katakana1",
+          "symbol": "プ",
+          "romanji": "pu"
+        },
+        {
+          "type": "katakana1",
+          "symbol": "ペ",
+          "romanji": "pe"
+        },
+        {
+          "type": "katakana1",
+          "symbol": "ポ",
+          "romanji": "po"
+        },
+        {
+          "type": "katakana2",
+          "symbol": "キャ",
+          "romanji": "kya"
+        },
+        {
+          "type": "katakana2",
+          "symbol": "キュ",
+          "romanji": "kyu"
+        },
+        {
+          "type": "katakana2",
+          "symbol": "キョ",
+          "romanji": "kyo"
+        },
+        {
+          "type": "katakana2",
+          "symbol": "ギャ",
+          "romanji": "gya"
+        },
+        {
+          "type": "katakana2",
+          "symbol": "ギュ",
+          "romanji": "gyu"
+        },
+        {
+          "type": "katakana2",
+          "symbol": "ギョ",
+          "romanji": "gyo"
+        },
+        {
+          "type": "katakana2",
+          "symbol": "シャ",
+          "romanji": "sha"
+        },
+        {
+          "type": "katakana2",
+          "symbol": "シュ",
+          "romanji": "shu"
+        },
+        {
+          "type": "katakana2",
+          "symbol": "ショ",
+          "romanji": "sho"
+        },
+        {
+          "type": "katakana2",
+          "symbol": "ジャ",
+          "romanji": "ja"
+        },
+        {
+          "type": "katakana2",
+          "symbol": "ジュ",
+          "romanji": "ju"
+        },
+        {
+          "type": "katakana2",
+          "symbol": "ジョ",
+          "romanji": "jo"
+        },
+        {
+          "type": "katakana2",
+          "symbol": "ニャ",
+          "romanji": "nya"
+        },
+        {
+          "type": "katakana2",
+          "symbol": "ニュ",
+          "romanji": "nyu"
+        },
+        {
+          "type": "katakana2",
+          "symbol": "ニョ",
+          "romanji": "nyo"
+        },
+        {
+          "type": "katakana2",
+          "symbol": "ヒャ",
+          "romanji": "hya"
+        },
+        {
+          "type": "katakana2",
+          "symbol": "ヒュ",
+          "romanji": "hyu"
+        },
+        {
+          "type": "katakana2",
+          "symbol": "ヒョ",
+          "romanji": "hyo"
+        },
+        {
+          "type": "katakana2",
+          "symbol": "ビャ",
+          "romanji": "bya"
+        },
+        {
+          "type": "katakana2",
+          "symbol": "ビュ",
+          "romanji": "byu"
+        },
+        {
+          "type": "katakana2",
+          "symbol": "ビョ",
+          "romanji": "byo"
+        },
+        {
+          "type": "katakana2",
+          "symbol": "ピャ",
+          "romanji": "pya"
+        },
+        {
+          "type": "katakana2",
+          "symbol": "ピュ",
+          "romanji": "pyu"
+        },
+        {
+          "type": "katakana2",
+          "symbol": "ピョ",
+          "romanji": "pyo"
+        },
+        {
+          "type": "katakana2",
+          "symbol": "ミャ",
+          "romanji": "mya"
+        },
+        {
+          "type": "katakana2",
+          "symbol": "ミュ",
+          "romanji": "myu"
+        },
+        {
+          "type": "katakana2",
+          "symbol": "ミョ",
+          "romanji": "myo"
+        },
+        {
+          "type": "katakana2",
+          "symbol": "リャ",
+          "romanji": "rya"
+        },
+        {
+          "type": "katakana2",
+          "symbol": "リュ",
+          "romanji": "ryu"
+        },
+        {
+          "type": "katakana2",
+          "symbol": "リョ",
+          "romanji": "ryo"
+        },
+        {
+          "type": "katakana2",
+          "symbol": "チャ",
+          "romanji": "cha"
+        },
+        {
+          "type": "katakana2",
+          "symbol": "チュ",
+          "romanji": "chu"
+        },
+        {
+          "type": "katakana2",
+          "symbol": "チョ",
+          "romanji": "cho"
+        },
+        {
+          "type": "katakana2",
+          "symbol": "ヂャ",
+          "romanji": "dja"
+        },
+        {
+          "type": "katakana2",
+          "symbol": "ヂュ",
+          "romanji": "dju"
+        },
+        {
+          "type": "katakana2",
+          "symbol": "ヂョ",
+          "romanji": "djo"
         }
 ]
